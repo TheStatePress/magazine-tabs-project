4f65cde12714521559de940183c9084172dc9278
Unknown <kweinhold22@gmail.com>
2019-04-22 11:35:20 -0700

    Chuck's Guide

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import TabButton from './TabButton'
-import TabContainer from './TabContainer'
+import TabButton from './TabButton';
+import TabContainer from './TabContainer';
+import img1 from './bargraph.jpeg';
+import img2 from './usmap.png';
+// import beeg from url(https://www.google.com/imgres?imgurl=https%3A%2F%2Fpreview.redd.it%2Faoh4z6fwold21.png%3Fauto%3Dwebp%26s%3D235ccc0f2b4b68b909d18dea301fb828e4e79451&imgrefurl=https%3A%2F%2Fwww.reddit.com%2Fr%2Fvideogamedunkey%2Fcomments%2Falfh67%2Fbeeg_beeg_yoshi%2F&docid=H_4Zi1Xfi07Z5M&tbnid=yFINRX5trk4UNM%3A&vet=10ahUKEwjnzt2Hm9vhAhX7FTQIHeR8AFgQMwg_KAAwAA..i&w=420&h=413&bih=898&biw=840&q=beeg%20yoshi&ved=0ahUKEwjnzt2Hm9vhAhX7FTQIHeR8AFgQMwg_KAAwAA&iact=mrc&uact=8);
+//import logo from './logo.png'
 
 class App extends Component {
   constructor(props) {
@@ -31,24 +34,28 @@ class App extends Component {
           <TabButton tabNumber={2} currentTab={currentTab} onTabSwitch={this.makeOnTabSwitch(2)} label="Tab 2" />
           <TabButton tabNumber={3} currentTab={currentTab} onTabSwitch={this.makeOnTabSwitch(3)} label="Tab 3" />
           <TabButton tabNumber={4} currentTab={currentTab} onTabSwitch={this.makeOnTabSwitch(4)} label="Tab 4" />
-          <h2>we're on tab: {this.state.currentTab}</h2>
+          {/* <h2>we're on tab: {this.state.currentTab}</h2> */}
         </div>
         <div>
           <TabContainer currentTab={this.state.currentTab} tabNumber={1}>
-            this is tab one content
+            An important Atom Uwu
+            <img src={logo} alt='atom'></img>
+
           </TabContainer>
           <TabContainer currentTab={this.state.currentTab} tabNumber={2}>
             this is tab two content
+            <img src={img1} alt='graph'></img>
           </TabContainer>
           <TabContainer currentTab={this.state.currentTab} tabNumber={3}>
             this is tab three content
-        </TabContainer>
+            <img src={img2} width="500ev" alt="United States"></img>
+          </TabContainer>
           <TabContainer currentTab={this.state.currentTab} tabNumber={4}>
             this is tab four content
+            {/* <img src={beeg}></img> */}
         </TabContainer>
         </div>
       </div>
-
     );
   }
 }
