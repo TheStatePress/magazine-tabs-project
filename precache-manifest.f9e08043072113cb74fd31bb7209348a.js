61feb5603151eef573818cd69bc7b3eb245b9894
Chuck Dries <chuckd@axosoft.com>
2019-04-22 14:10:42 -0700

    Updates

---
+++ precache-manifest.f9e08043072113cb74fd31bb7209348a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc4025929c0103da35e4f9adac043122",
+    "url": "/magazine-tabs-project/static/media/usmap.fc402592.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/magazine-tabs-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ea252d4f6162ae8e476f",
+    "url": "/magazine-tabs-project/static/js/runtime~main.d325b1a2.js"
+  },
+  {
+    "revision": "8cc90566ae34a16bacb8",
+    "url": "/magazine-tabs-project/static/js/main.f2608d30.chunk.js"
+  },
+  {
+    "revision": "4ea327850c9ec1c21b9d",
+    "url": "/magazine-tabs-project/static/js/2.47a91485.chunk.js"
+  },
+  {
+    "revision": "8cc90566ae34a16bacb8",
+    "url": "/magazine-tabs-project/static/css/main.1b115bee.chunk.css"
+  },
+  {
+    "revision": "742b7c5201bda036a31e529ee82b7e9d",
+    "url": "/magazine-tabs-project/index.html"
+  }
+];
