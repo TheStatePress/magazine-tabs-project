ea9ec0874fd9b89b448466d2fde725830f8f798d
Unknown <kweinhold22@gmail.com>
2019-04-24 12:56:58 -0700

    Parker's Images, 3 tabs
    Finally. Also, 1 less tab

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import TabButton from './TabButton';
 import TabContainer from './TabContainer';
-import img1 from './bargraph.jpeg';
-import img2 from './usmap.png';
-import img3 from './iceland_map.png'
+import img1 from './pinkmap.png';
+import img2 from './dataset1.png';
+import img3 from './dataset2.png'
 //tab changes 2
 // import beeg from url(https://www.google.com/imgres?imgurl=https%3A%2F%2Fpreview.redd.it%2Faoh4z6fwold21.png%3Fauto%3Dwebp%26s%3D235ccc0f2b4b68b909d18dea301fb828e4e79451&imgrefurl=https%3A%2F%2Fwww.reddit.com%2Fr%2Fvideogamedunkey%2Fcomments%2Falfh67%2Fbeeg_beeg_yoshi%2F&docid=H_4Zi1Xfi07Z5M&tbnid=yFINRX5trk4UNM%3A&vet=10ahUKEwjnzt2Hm9vhAhX7FTQIHeR8AFgQMwg_KAAwAA..i&w=420&h=413&bih=898&biw=840&q=beeg%20yoshi&ved=0ahUKEwjnzt2Hm9vhAhX7FTQIHeR8AFgQMwg_KAAwAA&iact=mrc&uact=8);
 //import logo from './logo.png'
@@ -35,25 +35,24 @@ class App extends Component {
           <TabButton tabNumber={1} currentTab={currentTab} onTabSwitch={this.makeOnTabSwitch(1)} label="Tab 1" />
           <TabButton tabNumber={2} currentTab={currentTab} onTabSwitch={this.makeOnTabSwitch(2)} label="Tab 2" />
           <TabButton tabNumber={3} currentTab={currentTab} onTabSwitch={this.makeOnTabSwitch(3)} label="Tab 3" />
-          <TabButton tabNumber={4} currentTab={currentTab} onTabSwitch={this.makeOnTabSwitch(4)} label="Tab 4" />
+          {/* <TabButton tabNumber={4} currentTab={currentTab} onTabSwitch={this.makeOnTabSwitch(4)} label="Tab 4" /> */}
           {/* <h2>we're on tab: {this.state.currentTab}</h2> */}
         </div>
         <div>
           <TabContainer currentTab={this.state.currentTab} tabNumber={1}>
-            An important Atom Uwu
-            <img src={logo} wdith='500ev' alt='atom'></img>
-
+            <img src={img1} width='575ev' alt='graph'></img>
+            {/* <img src={logo} wdith='500ev' alt='atom'></img> */}
+            
           </TabContainer>
           <TabContainer currentTab={this.state.currentTab} tabNumber={2}>
-            <img src={img1} width='700ev' alt='graph'></img>
+            <img src={img2} width="650ev" alt="United States"></img>
           </TabContainer>
           <TabContainer currentTab={this.state.currentTab} tabNumber={3}>
-            <img src={img2} width="600ev" alt="United States"></img>
+          <img src={img3} width="575ev" alt="United States"></img>
           </TabContainer>
-          <TabContainer currentTab={this.state.currentTab} tabNumber={4}>
+          {/* <TabContainer currentTab={this.state.currentTab} tabNumber={4}>
           <img src={img3} width="650ev" alt="Iceland"></img>
-            {/* <img src={beeg}></img> */}
-        </TabContainer>
+        </TabContainer> */}
         </div>
       </div>
     );
